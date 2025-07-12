06fa9d08217eb078d5de32264a94dbf2daf33881
Opinder-singh19 <princesingh7589884958@gmail.com>
2025-07-12 14:10:45 +0530

    js work

---
+++ DAY11/3.js
@@ -0,0 +1,44 @@
+// switch case 
+
+/*var age=24;
+switch(true){
+    case  (age<18):
+        console.log("under age  ");
+        break;
+    
+    case  (age>=18&& age<60):
+        console.log("ok ");
+        break;
+    
+    case (age>=60):
+        console.log("elder one ");
+        break;
+    
+    
+    
+}*/
+
+//alert("hello sir ");     // for alert message 
+
+//conactination in alert box 
+//var a="Opinder"
+//alert("hello"+a)
+
+
+//confirm box 
+/*var a=confirm("DO you like our website ");
+if(a){
+    alert("Thanks");
+}
+else{
+    alert("Better luck next time ");
+}*/
+
+//prompt box
+
+//prompt("enter your name ");
+
+//using with alert box
+/*var a=prompt("enter your name ");
+alert(a);*/
+
